80d42924ab45086ec538224467a4acf19ba5538b
Shrikant Jadhav <shrikant.jadhav1@gmail.com>
2014-01-31 11:10:58 +0530

    Create Pairs.cpp
    https://www.hackerrank.com/challenges/pairs

---
+++ Pairs.cpp
@@ -0,0 +1,28 @@
+#include <set>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/* Head ends here */
+
+int main(){
+
+    int pairCount = 0;
+    int64_t el;
+    std::set<int64_t> s;
+    int N;
+    uint64_t K;
+    
+    std::cin>>N;
+    std::cin>>K;
+    
+    while(N-- > 0) {
+        std::cin>>el;
+        if (s.count(el+K) > 0 || s.count(el-K) > 0) 
+            pairCount = pairCount + s.count(el+K) + s.count(el-K);
+        s.insert(el);
+    }
+    
+    std::cout<<pairCount;
+return 0;
+}
